726e310d934d7e12d4b7197f56912013808c0c9e
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-12-25 11:56:24 +0100

    Handle SIGINT and SIGTERM signals

--- Sources/CLI/Command/Run.swift
+++ Sources/CLI/Command/Run.swift
@@ -1,5 +1,6 @@
 import Console
 import DotEnv
+import Foundation
 
 extension CLI.Chaqmoq {
     /// A command to run an application.
@@ -28,7 +29,36 @@ extension CLI.Chaqmoq {
             let environment = self.environment ?? "development"
             let dotEnv = DotEnv()
             dotEnv.set(environment, forKey: "CHAQMOQ_ENV")
+
+            let queue = DispatchQueue(label: "dev.chaqmoq.cli.shutdown")
+            let terminationSignal = makeSignal(SIGTERM, on: queue)
+            terminationSignal.resume()
+            let interruptionSignal = makeSignal(SIGINT, on: queue)
+            interruptionSignal.resume()
+
             CLI.shell("swift", "run")
+        }
+
+        private func makeSignal(_ code: Int32, on queue: DispatchQueue? = nil) -> DispatchSourceSignal {
+            let signalSource = DispatchSource.makeSignalSource(signal: code, queue: queue)
+            signalSource.setEventHandler {
+                let port = 8080
+                let outputPipe = Pipe()
+                let process = Process()
+                process.launchPath = "/usr/bin/env"
+                process.arguments = ["lsof", "-t", "-i:\(port)"]
+                process.standardOutput = outputPipe
+                process.waitUntilExit()
+                process.launch()
+
+                let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+                let pid = String(decoding: outputData, as: UTF8.self).trimmingCharacters(in: .newlines)
+
+                CLI.shell("kill", "-9", "\(pid)")
+            }
+            signal(code, SIG_IGN)
+
+            return signalSource
         }
     }
 }
